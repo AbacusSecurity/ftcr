e3773583ed93d3f41573f163e57f7700855b6ad6
AbacusSecurity <secureabacus@gmail.com>
2022-06-25 12:12:07 +0200

    first commit

---
+++ ios/Classes/FtcrPlugin.m
@@ -0,0 +1,15 @@
+#import "FtcrPlugin.h"
+#if __has_include(<ftcr/ftcr-Swift.h>)
+#import <ftcr/ftcr-Swift.h>
+#else
+// Support project import fallback if the generated compatibility header
+// is not copied when this plugin is created as a library.
+// https://forums.swift.org/t/swift-static-libraries-dont-copy-generated-objective-c-header/19816
+#import "ftcr-Swift.h"
+#endif
+
+@implementation FtcrPlugin
++ (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
+  [SwiftFtcrPlugin registerWithRegistrar:registrar];
+}
+@end
